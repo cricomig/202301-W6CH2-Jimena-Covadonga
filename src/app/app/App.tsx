4651a049e003d8103f8ba30841be6983ee777553
Covadonga M <cova.millan@gmail.com>
2023-02-21 22:06:41 +0100

    Update add file

--- src/app/app/App.tsx
+++ src/app/app/App.tsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Cards></Cards>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
